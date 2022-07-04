b4510091fefdd9c8039ff6005b6804e0eb6143f2
zhangsan <zhangsan@atguigu.com>
2022-07-04 20:59:11 +0800

    master commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -17,6 +17,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git2");
-        System.out.println("hello git4");
+        System.out.println("master test");
     }
 }
