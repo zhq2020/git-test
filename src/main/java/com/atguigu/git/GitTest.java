a82363bb993c03f4742f78a6d1210293f5e262d7
zhq2020 <73781961+zhq2020@users.noreply.github.com>
2022-07-04 22:26:53 +0800

    Github项目更新了

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -21,5 +21,6 @@ public class GitTest {
         System.out.println("修改了快捷方式");
         System.out.println("修改了快捷方式2");
         System.out.println("pull test");
+        System.out.println("Github项目更新了");
     }
 }
