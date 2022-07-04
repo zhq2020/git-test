4901067204f581b4741083f4881df9ac2777d65f
zhangsan <zhangsan@atguigu.com>
2022-07-04 20:42:07 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,20 @@
+package com.atguigu.git;
+
+/**
+ * @ProjectName: git-test
+ * @Package: com.atguigu.git
+ * @ClassName: GitTest
+ * @Description: java类作用描述
+ * @Author: 周思
+ * @CreateDate: 2022/7/4 20:38
+ * @UpdateUser: 更新者
+ * @UpdateDate: 2022/7/4 20:38
+ * @UpdateRemark: 更新说明
+ * @Version: 1.0
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
